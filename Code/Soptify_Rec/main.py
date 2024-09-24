26637d4b4eb4951c2d3c3c8621c336fb0ace9525
Christopher yang <146152532+Cyang18@users.noreply.github.com>
2024-09-23 16:35:40 -0800

    Update main.py

--- Code/Soptify_Rec/main.py
+++ Code/Soptify_Rec/main.py
@@ -6,4 +6,4 @@ load_dotenv()
 client_id = os.getenv("CLIENT_ID")
 client_secert = os.getenv("CLIENT_SECERT")
 
-print(client_id, client_secert)
+# print(client_id, client_secert) #uncomment to see if your program was able to get your Client ID and secret ID from .env
