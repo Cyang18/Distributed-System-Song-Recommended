f28c9448a216150ed95d96cc6e34050e774bcf01
Christopher yang <146152532+Cyang18@users.noreply.github.com>
2024-10-05 22:36:52 -0800

    Create TopTracks.js

---
+++ ToGetData/TopTracks.js
@@ -0,0 +1,25 @@
+const token = '';
+async function fetchWebApi(endpoint, method, body) {
+  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    method,
+    body:JSON.stringify(body)
+  });
+  return await res.json();
+}
+
+async function getTopTracks(){
+  return (await fetchWebApi(
+    'v1/me/top/tracks?time_range=long_term&limit=5', 'GET'
+  )).items;
+}
+
+const topTracks = await getTopTracks();
+console.log(
+  topTracks?.map(
+    ({name, artists}) =>
+      `${name} by ${artists.map(artist => artist.name).join(', ')}`
+  )
+);
