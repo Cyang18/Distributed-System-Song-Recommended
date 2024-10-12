716601131b6cb2c85f107594bc3a7c523d22ebea
AlwaysJed <161187377+AlwaysJed@users.noreply.github.com>
2024-10-11 19:25:34 -0800

    Update ExtractingPersonalrec.js
    Async Function

--- ToGetData/ExtractingPersonalrec.js
+++ ToGetData/ExtractingPersonalrec.js
@@ -1,1 +1,12 @@
+const token = '';
+
+const fs = require('fs'); 
+
+async function fetchWebApi(endpoint, method, body) {
+  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+
 
