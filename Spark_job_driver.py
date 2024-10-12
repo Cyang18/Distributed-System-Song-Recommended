ab0cf367d73a18c23c888eaa6dbbe8b0c55d0455
Christopher yang <146152532+Cyang18@users.noreply.github.com>
2024-10-12 15:36:30 -0800

    Update Spark_job_driver.py

--- Spark_job_driver.py
+++ Spark_job_driver.py
@@ -3,6 +3,10 @@ from pyspark.sql import Row
 
 spark = SparkSession.builder.enableHiveSupport().getOrCreate()
 
+# optional: reading in the data file
+# json_file_path = 'path_to_your_json_file.json'
+# data = spark.read.json(json_file_path)
+
 data = [ ] #place data here from or in bucket
 row_data = [
     Row(
@@ -14,7 +18,7 @@ row_data = [
 
 
 df = spark.createDataFrame(row_data)
-df.write.mode("overwrite").saveAsTable("music_tracks")
+df.write.mode("overwrite").saveAsTable("music_tracks") # Change the name of the table to ur choice
 print("Data saved to Hive table 'music_tracks'")
 
 
