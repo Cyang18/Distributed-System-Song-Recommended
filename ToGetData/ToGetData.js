73fa549ad0c125b8960a365b8e8afdcc37deef3a
ulalenepi <ulalenepi@alaska.edu>
2024-10-14 02:44:42 -0800

    Update ToGetData.js

--- ToGetData/ToGetData.js
+++ ToGetData/ToGetData.js
@@ -1,7 +1,17 @@
 /*
-Define function fetchWebApi
+Provide a token for authorization and set it as an empty string
 
-Define function getTopTracks.
+Define function fetchWebApi
+   - it will have the  parameters:
+    - endpoint
+    - method
+    - body
+
+Define function getTopTracks.:
+   - Call fetchWebApi with the following parameters:
+     - endpoint: 'v1/me/top/tracks?time_range=long_term&limit=5'
+     - method: 'GET'
+   - Return the items from the response.
 
 Call getTopTracks and store the result in topTracks.
 
@@ -11,6 +21,5 @@ For each track in topTracks:
    - Join the artist names with commas.
    - Log the track name and the joined artist names in the format:
      "Track Name by Artist1, Artist2, ...".
-
-   */
+*/
 
