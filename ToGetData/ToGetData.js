655c1042e1dfd96b7c0d88532adeed3dfb081331
ulalenepi <ulalenepi@alaska.edu>
2024-10-14 02:03:29 -0800

    Update ToGetData.js

--- ToGetData/ToGetData.js
+++ ToGetData/ToGetData.js
@@ -1,1 +1,16 @@
+/*
+Define function fetchWebApi
+
+Define function getTopTracks.
+
+Call getTopTracks and store the result in topTracks.
+
+For each track in topTracks:
+   - Extract the track name.
+   - Extract the names of the artists.
+   - Join the artist names with commas.
+   - Log the track name and the joined artist names in the format:
+     "Track Name by Artist1, Artist2, ...".
+
+   */
 
